2d85ebee539a79bcd12c92f82462b16812efe364
BushevAndrey <chhufudud@gmail.com>
2024-02-16 21:31:55 +0300

    Добавьте файлы проекта.

---
+++ WpfApp19/Page1.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp19;
+
+namespace WpfApp19
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    class N
+    {
+        public void Resh(int a)
+        {
+            if (a % 10 == 7)
+            {
+                MessageBox.Show("Число оканчивается на 7");
+            }
+            else
+            {
+                MessageBox.Show("Число не оканчивается на 7");
+            }
+        }
+    }
+    public partial class Page1 : Page
+    {
+        int X;
+        N n = new N();
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            X = int.Parse(AA.Text);
+            n.Resh(X);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Page2(X));
+        }
+    }
+}
---
+++ WpfApp19/Page2.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp19
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    class Res
+    {
+        public void R(int a)
+        {
+            if (a >= 0)
+            {
+                a -= 8;
+            }
+            else
+            {
+                a += 6;
+            }
+            MessageBox.Show($"Ответ: {a}");
+        }
+    }
+    public partial class Page2 : Page
+    {
+        int X;
+        int K;
+        Res res = new Res();
+        public Page2(int x)
+        {
+            InitializeComponent();
+            X = x;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            K = int.Parse(AA.Text);
+            res.R(K);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Сумма: {X + K}");
+        }
+    }
+}
